27f0d63ee37f6fb984d69fe3bbdc34ae38ce0d13
3103ski <bjastski@gmail.com>
2020-09-19 12:34:08 -0700

    changes modal data attr to jQuery

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,11 @@
 $(function () {
 	$('[data-toggle="tooltip"]').tooltip();
+
+	$('#reserveButton').click(function () {
+		$('#reserveModal').modal('show');
+	});
+
+	$('#loginButton').click(function () {
+		$('#loginModal').modal('show');
+	});
 });
